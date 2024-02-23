e25e81d735a627e74bfced0c4d80e66fefa4ad04
angeliquelau <angeliquelau1201@gmail.com>
2024-02-23 14:09:23 +0800

    Initial Commit

---
+++ Countdown/CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownApp.swift
+//  Countdown
+//
+//  Created by Angelique Lau on 23/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
